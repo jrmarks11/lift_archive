924473e513371ecc1143a5a74a39b07e90dfdf24
John Marks <jmarks@covermymeds.com>
2017-03-05 15:19:32 -0500

    Navbar added to layout and coherence

--- web/views/coherence/layout_view.ex
+++ web/views/coherence/layout_view.ex
@@ -1,5 +1,14 @@
 defmodule Coherence.LayoutView do
-  use Lift.Coherence.Web, :view
+use Phoenix.View, root: "web/templates"
+      # Import convenience functions from controllers
+      import Phoenix.Controller, only: [get_csrf_token: 0, get_flash: 2, view_module: 1]
 
+      # Use all HTML functionality (forms, tags, etc)
+      use Phoenix.HTML
+
+      import Lift.Router.Helpers
+      import Lift.ErrorHelpers
+      import Lift.Gettext
+      import Lift.Coherence.ViewHelpers
 
 end
